bdea5544b7f94340280734279531827e2305fde7
Serhii Tarasenko <rootfellenua@gmail.com>
2020-11-25 18:21:50 +0400

    second commit

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ document.querySelector('.check').addEventListener('click', function () {
     displayMessage('Correct Number!');
     document.querySelector('.number').textContent = secretNumber; // to display secret number in the number area
     document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.number').style.width = '30em';
+    document.querySelector('.number').style.width = '30rem';
     if (score > highscore) {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
